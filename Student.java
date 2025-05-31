9713bd5bce280ffe6e1cb7b9d5274f16ef7a4dd2
ynadaf <ynadaf1975@gmail.com>
2025-05-31 23:14:21 +0530

    Main

---
+++ Student.java
@@ -0,0 +1,11 @@
+public class Student {
+public static void main(String[] args){
+    String name="saniya";
+    int Birthyear=2005;
+    int currentyear=2025;
+    int age=currentyear-Birthyear;
+
+    System.out.println("Name:"+name);
+    System.out.println("Age:"+age);
+}
+}
